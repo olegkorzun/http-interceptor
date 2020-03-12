9ac7b1c030945f434599a034281efd43900f479c
oleg korzun <traffitech1@gmail.com>
2020-03-12 18:00:49 +0200

    first version
    first version

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit} from '@angular/core'
+import {Todo, TodosService} from './todos.service'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  todos: Todo[] = []
+  loading = false
+  todoTitle = ''
+  error = ''
+  constructor(private todosService: TodosService) {}
+  ngOnInit() { this.fetchTodos() }
+  fetchTodos() {
+    this.loading = true
+    this.todosService.fetchTodos()
+      .subscribe(todos => {
+        this.todos = todos
+        this.loading = false
+      }, error => { this.error = error.message })
+  }
+}
+
+  /*
+    addTodo() {
+    if (!this.todoTitle.trim()) { return }
+    this.todosService.addTodo({
+      title: this.todoTitle,
+      completed: false
+    }).subscribe(todo => {
+      this.todos.push(todo)
+      this.todoTitle = ''
+    })
+  } 
+  removeTodo(id: number) {
+    this.todosService.removeTodo(id)
+      .subscribe(() => {
+        this.todos = this.todos.filter(t => t.id !== id)
+      })
+  }
+  completeTodo(id: number) {
+    this.todosService.completeTodo(id).subscribe(todo => {
+      this.todos.find(t => t.id === todo.id).completed = true
+    })
+  }
+  */
+
